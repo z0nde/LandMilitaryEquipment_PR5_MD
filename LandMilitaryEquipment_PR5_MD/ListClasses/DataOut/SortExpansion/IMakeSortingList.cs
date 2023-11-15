c37cd80586b7da06d55b1d63fefc5c1b0e222b04
artem <artem@Z0NDE>
2023-11-15 10:19:35 +0300

    Sorting

---
+++ LandMilitaryEquipment_PR5_MD/ListClasses/DataOut/SortExpansion/IMakeSortingList.cs
@@ -0,0 +1,10 @@
+﻿using LandMilitaryEquipment_PR5_MD.Classes;
+using LandMilitaryEquipment_PR5_MD.ListClasses.ListDataInteractions;
+
+namespace LandMilitaryEquipment_PR5_MD.ListClasses.DataOut.SortExpansion
+{
+    public interface IOutputSorting
+    {
+        public List<LandMilitaryEquipment> OutData(ListDataLandMilitaryEquipment listData);
+    }
+}
---
+++ LandMilitaryEquipment_PR5_MD/ListClasses/DataOut/SortExpansion/ISoringOutput.cs
@@ -0,0 +1,6 @@
+﻿namespace LandMilitaryEquipment_PR5_MD.ListClasses.DataOut.SortExpansion;
+
+public class ISoringOutput
+{
+    
+}
